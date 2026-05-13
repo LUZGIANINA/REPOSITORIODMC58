dd825e61886225f1be268bdcbc05ad1fe4f6c7ba
LUZGIANINA <peqush@gmail.com>
2026-05-12 20:02:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ st.title("Mi primera aplicación en Python")
 st.sidebar.title ("Parámetros")
 
 st.write("Elaborado por Luz Garcia")
+
+Sesion = st.sidebar.selectbox("seeccione una sesion,["Sesion 1","Sesion 2", "Sesion 3", "Sesion 4"])
