11f78fe88bf62cc3bfedc24495296bb6368dcb9e
LUZGIANINA <peqush@gmail.com>
2026-05-14 21:16:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
+import numpy as np
 
 st.title("Mi primera aplicación en python")
 
 st.sidebar.title("Parámetros")
 
-st.write("Elaborado por: Luz Garcia")
+st.write("Elaborado por: Carlos Carrillo")
 
 st.sidebar.image("DMC.png")
 
@@ -25,9 +26,23 @@ elif sesion == "Sesión 2":
   st.write("El precio final del producto es: ", precio_final_producto  )
 
 
+elif sesion == "Sesión 3":
+  st.write("Bienvenido la sesión 3")
+  
+  fin_rango = st.slider("Selecione un valor",min_value = 0 , max_value=20, value =7 )
+
+  arreglo = np.arange(0 , fin_rango)
+
+  st.write(arreglo)
+
+
+
+else:
+  st.write("Bienvenido la sesión 4")
 
 elif sesion == "Sesión 3":
   st.write("Bienvenido la sesión 3")
+  
 
 else:
   st.write("Bienvenido la sesión 4")
